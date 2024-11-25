30b5052ccbc0efd9136d9312a5bc8ffb54b101d6
Ariana <aripapantonio@gmail.com>
2024-11-25 23:18:35 +0100

    Fase 1

---
+++ main.py
@@ -0,0 +1,6 @@
+## Importacíón librerias
+
+import pandas as pd
+import numpy as np
+from src import soporte_funciones as spt
+
---
+++ src/soporte_funciones.py
@@ -0,0 +1,84 @@
+
+## Importación Librerias
+
+import pandas as pd
+import os
+import sys
+#from IPython.display import display
+
+## Funciones EDA y transformación de datos: 
+
+def exploracion(df):
+
+## "Función exploración DF (pocentaje de nulos, tipo de datos, valores únicos, cantidad de registros duplicados y principales estadisticas)" #En el caso de duplicados, cambiamos a keep=False para que cuente todas las ocurrencias). 
+
+    df_info = pd.DataFrame()
+
+    df_info["% nulos"] = round(df.isna().sum()/df.shape[0]*100, 2).astype(str)+"%"
+    df_info["% no_nulos"] = round(df.notna().sum()/df.shape[0]*100, 2).astype(str)+"%"
+    df_info["tipo_dato"] = df.dtypes
+    df_info["num_valores_unicos"] = df.nunique()
+
+    print(f"""El DataFrame tiene {df.shape[0]} filas y {df.shape[1]} columnas.
+Tiene {df.duplicated().sum()} datos duplicados, lo que supone un porcentaje de {round(df.duplicated().sum()/df.shape[0]*100,2)}% de los datos. 
+
+Hay {len(list(df_info[(df_info["% nulos"] != "0.0%")].index))} columnas con datos nulos, y son: 
+{list(df_info[(df_info["% nulos"] != "0.0%")].index)}
+
+y sin nulos hay {len(list(df_info[(df_info["% nulos"] == "0.0%")].index))} columnas y son: 
+{list(df_info[(df_info["% nulos"] == "0.0%")].index)}
+
+
+A continuación tienes un detalle sobre los datos nulos y los tipos y número de datos:""")
+    
+    display(df_info.head())
+
+    ## Comprobar si hay columnas categóricas y mostrar info de esas columnas
+    col_categoricas = df.select_dtypes(include = "O").columns
+    if len(col_categoricas) > 0: 
+        print("Principales estadísticos de las columnas categóricas:")
+        display(df.describe(include="O").T)
+    
+    else: 
+        print("\nEl dataframe no tiene columnas categóricas. \n")
+
+    ## Comprobar si hay columnas numéricas y mostrar info de esas columnas
+    col_numericas = df.select_dtypes(exclude = "O").columns
+    if len(col_numericas) > 0: 
+        print("Principales estadísticos de las columnas numéricas:")
+        display(df.describe(exclude="O").T)
+
+    return df_info
+
+
+
+
+## Función para cambiar valores negativos a positivos: 
+
+def negativos(num):
+
+    """
+    Función para cambiar valores negativos a positivos
+
+    """
+    return abs(num) 
+
+
+
+
+## Función para reemplazar espacios por guiones bajos y a minusculas: 
+
+def espacios_guion_bajo(df):
+    """
+    Función para reemplazar los espacios en las columnas por guiones bajos 
+    y convertirlas a minúsculas.
+    
+    Parámetros:
+    df (DataFrame): DF cuyas columnas se desean modificar.
+
+    Devuelve:
+    DataFrame: DF con los nombres de las columnas modificados.
+    """
+    # Convertir nombres de columnas a minúsculas y reemplazar espacios por guiones bajos
+    df.columns = [col.lower().replace(" ", "_") for col in df.columns]
+    return df
