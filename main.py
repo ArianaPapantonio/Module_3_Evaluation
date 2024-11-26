92f8a58517bba060e048a5cb4350dc4fc31716ca
Ariana <aripapantonio@gmail.com>
2024-11-26 10:19:11 +0100

    Descripción de los cambios realizados

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 ## Importacíón librerias
-
+#%%
 import pandas as pd
 import numpy as np
 from src import soporte_funciones as spt
 
+#%%
+prueba_main = pd.read_csv('files/filas_duplicadas.csv')
+#%%
+spt.exploracion(prueba_main)
+# %%
